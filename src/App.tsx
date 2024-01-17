292a54320192e5fafc778232aa4e7441e0e97a47
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2024-01-17 16:31:47 +1300

    Custom Content Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css"
 import Application from "./components/application/application"
 import Skills from "./components/skills/skills"
+import Counter from "./components/counter/counter"
+import AppProviders from "./providers/app-providers"
+import MuiMode from "./components/mui/mui-mode"
 
 function App() {
   return (
-    <div className='App'>
-      <Application />
-      <Skills skills={["HTML", "CSS", "JavaScript"]} />
-    </div>
+    <AppProviders>
+      <div className='App'>
+        <Application />
+        <Skills skills={["HTML", "CSS", "JavaScript"]} />
+        <Counter />
+        <MuiMode />
+      </div>
+    </AppProviders>
   )
 }
 
